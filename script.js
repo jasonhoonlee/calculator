7b1a529e1ef27c31a5a23530f0b2c9affa001fef
Hoon Lee <jasonhoonlee@gmail.com>
2022-06-16 20:51:33 -0700

    For buttons other than numbers and operators, use getElementById method.

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ let currentOperation = null;
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operator]');
-const percentButton = document.querySelector('#percentBtn');
-const pointButton = document.querySelector('#pointBtn');
-const signButton = document.querySelector('#signBtn');
-const deleteButton = document.querySelector('#deleteBtn');
-const clearButton = document.querySelector('#clearBtn');
-const equalsButton = document.querySelector('#equalsBtn');
 
+const percentButton = document.getElementById('#percentBtn');
+const pointButton = document.getElementById('#pointBtn');
+const signButton = document.getElementById('#signBtn');
+const deleteButton = document.getElementById('#deleteBtn');
+const clearButton = document.getElementById('#clearBtn');
+const equalsButton = document.getElementById('#equalsBtn');
 
+const currentOperationScreen = document.getElementById('#currentOperationScreen');
+const lastOperationScreen = document.getElementById('#lastOperationScreen')
 
 
