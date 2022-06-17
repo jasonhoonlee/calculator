cbdff0e0f9628c39f64f7dd4000d8c166df9e4f3
Hoon Lee <jasonhoonlee@gmail.com>
2022-06-16 20:10:42 -0700

    Create variables for first and second operands. Create querySelectors for buttons.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let firstOperand = '';
+let lastOperand = '';
+let currentOperation = null;
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const percentButton = document.querySelector('#percentBtn');
+const pointButton = document.querySelector('#pointBtn');
+const signButton = document.querySelector('#signBtn');
+const deleteButton = document.querySelector('#deleteBtn');
+const clearButton = document.querySelector('#clearBtn');
+const equalsButton = document.querySelector('#equalsBtn');
+
+
+
+
